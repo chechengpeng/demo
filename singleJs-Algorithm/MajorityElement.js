520265f06a1f75ebaef6e4f8fdcf88ea8c2d2901
chechengpeng <chechengpeng@126.com>
2016-12-14 15:49:43 +0800

    add MajorityElement2

--- singleJs-Algorithm/MajorityElement.js
+++ singleJs-Algorithm/MajorityElement.js
@@ -12,7 +12,7 @@
  * for循环内，嵌套了while循环，当元素过多时，执行时间超时了
  * 算法之所以为算法，就是要想出一套简单快速的方法，而不是错误的套路，一开始光想着解决思路是统计数组元素出现的次数，使用indexOf方法来实现，
  * 而题目中已经说了，这是一个一定存在的数组，其实排下序，数组中间的那个数就是。
- * majorityElement1是最近做的，都不知道自己当时是怎么想出第二种方法的，智商在下降啊。
+ * majorityElement1是最近做的，都不知道自己当时是怎么想出第三种方法的，智商在下降啊。
  */
 var majorityElement1 = function(nums) {
   var times = nums.length/2;
@@ -32,6 +32,15 @@ var majorityElement1 = function(nums) {
 };
 
 var majorityElement2 = function(nums) {
+  for(var i=0;i<nums.length;i++){
+    var arr = nums.filter(function(a){return a==nums[i]});
+    if(arr.length>nums.length/2){
+      return nums[i]
+    }
+  }
+};
+
+var majorityElement3 = function(nums) {
   var arr = nums.sort(function(a,b){return a-b});
   if(arr.length>1){
     return arr[Math.floor(arr.length/2)];
