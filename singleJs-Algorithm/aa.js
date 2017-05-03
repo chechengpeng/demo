acdc319b61b9639b63b05b36cb4ec8a5ee00f814
chechengpeng <chechengpeng@126.com>
2017-05-03 18:20:43 +0800

    day day algor

---
+++ singleJs-Algorithm/aa.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {number}
+ */
+var findLUSlength = function (a, b) {
+  var c = b;
+  var d = a;
+  if (a.length > b.length) {
+    a = c;
+    b = d;
+  }
+  console.log(a,b)
+  if (b.length - b.replace(a, '').length == a.length) {
+    return -1
+  }
+  var arr = [1];
+  var i = 1;
+  var j = 0;
+  while(i<a.length){
+    while(!b.includes(a.substring(j,i)) && i<a.length){
+      i++;
+    }
+    arr.push(i)
+    j=i;
+    i++;
+  }
+console.log( Math.max.apply(null, arr))
+return Math.max.apply(null, arr)
+};
+
+findLUSlength('abaefff','cdcf')
