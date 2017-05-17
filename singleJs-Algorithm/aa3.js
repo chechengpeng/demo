352330f0e4008187cb4ac1f46b946ebf4032d278
chechengpeng <chechengpeng@126.com>
2017-05-17 18:25:42 +0800

    wrong

---
+++ singleJs-Algorithm/aa3.js
@@ -0,0 +1,11 @@
+var searchInsert = function(nums, target) {
+     if(nums.indexOf(target) == -1){
+         var mid = Math.floor(nums.length/2);
+         if(nums[mind]>target){
+             nums = nums.slice(0,mid)
+         }else{
+             nums = nums.slice(mid)
+         }
+     }
+
+};
