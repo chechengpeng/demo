c6bf804fc4783c35cf056667350cda2bc8f9cf5e
chechengpeng <chechengpeng@126.com>
2018-01-11 15:33:40 +0800

    放弃二叉树

--- singleJs-Algorithm/617. Merge Two Binary Trees.js
+++ singleJs-Algorithm/617. Merge Two Binary Trees.js
@@ -15,8 +15,11 @@
 var mergeTrees = function(t1, t2) {
   var arr1 = toArray(t1);
   var arr2 = toArray(t2);
+  console.log("arr1", arr1);
+  console.log("arr2", arr2);
   var arrA = mergeArr(arr1, arr2);
-  console.log(arrA);
+  console.log("arrA", arrA.length);
+  return toTree(arrA);
 };
 function toArray(node) {
   if (!node) {
@@ -32,7 +35,6 @@ function toArray(node) {
       } else {
         arr.push(null);
       }
-      console.log("node", node);
       if (node && node.left) {
         que.push(node.left);
       } else {
@@ -49,10 +51,14 @@ function toArray(node) {
 }
 function mergeArr(a1, a2) {
   var newArr = [];
-  var l1 = a1.length;
-  var l2 = a2.length;
+  if (!a1 && !a2) {
+    return [null];
+  }
+  var l1 = a1 ? a1.length : 0;
+  var l2 = a2 ? a2.length : 0;
+
   if (l1 > l2) {
-    var oArr = a1.slice(l2 - 1);
+    var oArr = a1.slice(l2);
     for (let i = 0; i < l2; i++) {
       if (a1[i] === null && a2[i] === null) {
         newArr.push(null);
@@ -66,7 +72,7 @@ function mergeArr(a1, a2) {
     }
     return [...newArr, ...oArr];
   } else {
-    var oArr = a2.slice(l1 - 1);
+    var oArr = a2.slice(l1);
     for (let i = 0; i < l1; i++) {
       if (a1[i] === null && a2[i] === null) {
         newArr.push(null);
@@ -81,42 +87,39 @@ function mergeArr(a1, a2) {
     return [...newArr, ...oArr];
   }
 }
-var arr = [3, 4, 5, 5, 4, null, 7];
-toTree(arr);
 function toTree(arr) {
-  var bst = new BST();
+  let binaryTree = new BinaryTree();
   for (var i = 0; i < arr.length; i++) {
-    bst.insert(arr[i]);
+    binaryTree.insert(arr[i]);
   }
-  console.log("bst", bst);
+  return binaryTree.root;
 }
-function BST() {
-  this.root = null;
+
+function BinaryTree() {
+  this.root = null; // 树根
+  this.queue = []; // 存储会被使用的父节点
+  this.insertNum = 0; // 记录插入操作的次数
   this.insert = insert;
 }
-function TreeNode(val) {
-  this.val = val;
-  this.left = this.right = null;
-}
-function insert(val) {
-  var n = new TreeNode(val);
-  if (this.root == null) {
-    this.root = n;
+function insert(value) {
+  this.insertNum++; // 插入次数加1
+  let node = !value && typeof value === "object" ? null : new TreeNode(value); // 判断是否为空节点
+  if (!this.root) {
+    // 判断根节点是否存在
+    this.root = node; // 插入根节点
+    node && this.queue.push(this.root); // 非空节点入列
   } else {
-    var current = this.root;
-    var parent;
-    while (true) {
-      parent = current;
-      var left = current.left;
-      var right = current.right;
-      if (left == null) {
-        parent.left = n;
-        break;
-      }
-      if (right == null) {
-        parent.right = n;
-        break;
-      }
+    // 插入非根节点
+    let parent = this.queue[0]; // 被插入的父节点
+    if (!(this.insertNum % 2)) {
+      // 通过插入次数判断左右
+      parent.left = node; // 插入左边
+      node && this.queue.push(parent.left); // 非空节点入列
+    } else {
+      parent.right = node; // 插入右边
+      node && this.queue.push(parent.right); // 非空节点入列
+      this.queue.shift(); // 当前父节点parent 已经不可能再插入子节点，故出列
     }
   }
+  return this;
 }
